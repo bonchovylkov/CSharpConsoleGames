9fd8905c0e2210bb26db7e059935e494b1ec72b2
bonchovylkov <bonchovylkov>
2020-01-11 11:22:26 +0200

    Add abstract GameObject and interace IGameObject

---
+++ Snake/GameDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace Snake
+{
+    public enum GameDisplay
+    {
+        [Description("*")]
+        SnakeBody,
+        [Description(">")]
+        SnakeHeadRight,
+        [Description("<")]
+        SnakeHeadLeft,
+        [Description("^")]
+        SnakeHeadUp,
+        [Description("v")]
+        SnakeHeadDown,
+        [Description("@")]
+        Food,
+        [Description("=")]
+        Obstacle,
+    }
+}
---
+++ Snake/GameObject.cs
@@ -0,0 +1,21 @@
+﻿using Snake.Extentions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    public abstract class GameObject : IGameObject
+    {
+        public Position Position { get; set; }
+
+        public GameDisplay Display { get; set; }
+
+        public bool CollideWith(IGameObject otherGameObject)
+        {
+            return this.Position.Equals(otherGameObject.Position);
+        }
+    }
+}
